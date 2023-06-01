40bfa6adfced9837b486519d9c7e243663375b7e
Cristina'S <119589958+cristinascripciuc@users.noreply.github.com>
2023-06-01 09:59:54 +0200

    Update login.component.ts

--- pizzeria-app/src/app/login/login.component.ts
+++ pizzeria-app/src/app/login/login.component.ts
@@ -1,31 +1,4 @@
-/*import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { GestioneUtentiService } from '../gestione-utenti.service';
 
-
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
-})
-export class LoginComponent {
-
-  constructor(private userServ: GestioneUtentiService, private router: Router){
-  }
-
-  loggedIn: boolean | undefined;
-
-  onLogin(){
-    this.loggedIn = true;
-    this.userServ.login();
-    this.router.navigate(['/']);
-  }
-
-  onLogout(){
-    this.loggedIn = false;
-    this.userServ.logout();
-  }
-}*/
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { GestioneUtentiService } from '../gestione-utenti.service';
